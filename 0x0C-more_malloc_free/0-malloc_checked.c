7a07ae4dc20f63bb070361c3b4403558e3b3bf56
ivasha <you@eliakim.com>
2023-07-15 11:00:50 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,29 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void* malloc_checked(size_t size)
-{
-    void* ptr = malloc(size);
-    if (ptr == NULL) {
-        printf("malloc failed! Terminating process with status value 98.\n");
-        exit(98);
-    }
-    return ptr;
-}
-
-int main()
-{
-    // Example usage of malloc_checked
-    int* arr = malloc_checked(10 * sizeof(int));
-    
-    // Use the allocated memory
-    for (int i = 0; i < 10; i++) {
-        arr[i] = i;
-    }
-    
-    // Free the allocated memory
-    free(arr);
-    
-    return 0;
-}
-
+#include <stdlib.h> 
+ #include "main.h" 
+  
+ /** 
+  * *malloc_checked - allocates memory using malloc 
+  * @b: number of bytes to allocate 
+  * 
+  * Return: a pointer to the allocated memory 
+  */ 
+ void *malloc_checked(unsigned int b) 
+ { 
+         void *ptr; 
+  
+         ptr = malloc(b); 
+  
+         if (ptr == NULL) 
+                 exit(98); 
+  
+         return (ptr); 
+ }

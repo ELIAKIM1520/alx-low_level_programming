e1a622f134fb108878c179960efab6188ad9d101
ivasha <you@eliakim.com>
2023-08-01 04:44:07 -0700

    git add

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,65 +1,25 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
- * _ra - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
+ *add_nodeint - Adds a node to a single linked list
+ * @head: A pointer to the pointer to the first element.
+ * @n: The integer to initialize with.
+ * Return: The address of the item just added.
  */
-listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t **newlist;
-	size_t i;
+	listint_t *l;
 
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
+	l = malloc(sizeof(listint_t));
+	if (l)
 	{
-		free(list);
-		exit(98);
+		l->n = n;
+		l->next = *head;
+		*head = l;
 	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
+	return (l);
 }
-
-/**
- * free_listint_safe - frees a listint_t linked list.
- * @head: double pointer to the start of the list
- *
- * Return: the number of nodes in the list
- */
-size_t free_listint_safe(listint_t **head)
-{
-	size_t i, num = 0;
-	listint_t **list = NULL;
-	listint_t *next;
-
-	if (head == NULL || *head == NULL)
-		return (num);
-	while (*head != NULL)
-	{
-		for (i = 0; i < num; i++)
-		{
-			if (*head == list[i])
-			{
-				*head = NULL;
-				free(list);
-				return (num);
-			}
-		}
-		num++;
-		list = _ra(list, num, *head);
-		next = (*head)->next;
-		free(*head);
-		*head = next;
-	}
-	free(list);
-	return (num);
 }

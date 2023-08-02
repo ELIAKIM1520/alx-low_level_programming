615f7bda18bc3cf5488fe04a42ed83bdfd2898dc
ivasha <you@eliakim.com>
2023-08-02 08:45:12 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,16 +1,18 @@
-#!/bin/bash
 #include "main.h"
+
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - This function checks for uppercase letters
+ * @c: character
+ * Return: 1 if uppercase 0 if lowercase
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
-
-	return (0);
+	{
+		return (0);
+	}
 }

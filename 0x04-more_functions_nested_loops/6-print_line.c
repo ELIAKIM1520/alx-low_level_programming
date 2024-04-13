31a42fd8e8ad8cb62154169964097dafa5a7da8d
ELIAKIM1520 <ivashaeliakim@gmail.com>
2024-04-13 14:47:09 +0000

    print line six

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+nclude "main.h"
+/**
+  * print_line - draws a straight line in terminal
+  * Return: void
+  * @n: value to pass
+  */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
